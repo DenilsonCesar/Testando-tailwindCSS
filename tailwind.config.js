64c6dc9d6df4fb8e2ead4e6d0f926d4cce025317
D3NIILSON <contato@sindsystem.com.br>
2024-01-18 21:46:57 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        brand: '#eb4034'
+      }
+    },
+  },
+  plugins: [],
+}
+
